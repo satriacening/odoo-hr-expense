e6946304d24dd066cc08abbefa935d588aec5de7
Satria Putra <satriaputra@Satrias-MacBook-Pro.local>
2022-10-05 16:18:37 +0700

    add new group in xml

--- addons/hr_expense_custom/__manifest__.py
+++ addons/hr_expense_custom/__manifest__.py
@@ -27,7 +27,9 @@ The whole flow is implemented as:
 This module also uses analytic accounting and is compatible with the invoice on timesheet module so that you are able to automatically re-invoice your customers' expenses if your work by project.
     """,
     'website': 'https://www.odoo.com/page/expenses',
-    'depends': ['hr_expense'],
+    'depends': ['hr_expense','hr',
+
+],
     'data': [
 
         # 'security/ir.model.access.csv',
--- addons/hr_expense_custom/models/hr_expense.py
+++ addons/hr_expense_custom/models/hr_expense.py
@@ -2,48 +2,56 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from email.policy import default
+from odoo.exceptions import UserError, ValidationError
+from odoo.tools import format_date
 import re
 
 from odoo import api, fields, models, _
 
 class HrExpense(models.Model):
-
     _inherit = ['hr.expense']
+
+    applicant_code = fields.Char(default=lambda self:self.env.user.name)
+    department_code = fields.Char(string="Departemen Code")
+    account = fields.Char(string="Account")
 
     destination = fields.Char(string='Destination')
     froms = fields.Char(string='From')
     to = fields.Char(string='To')
     total_amount = fields.Monetary(string="total")
     unit_amount = fields.Float(string="Amount")
+    round_type = fields.Selection([
+        ("oneway","Oneway"),
+        ("return","Return")
+    ])              
     payment_mode = fields.Selection(string="Payment", default="own_account", readonly=True)
-    
     def _compute_amount(self):
         for expense in self:
             expense.total_amount = expense.unit_amount
 
+
 class HrExpenseSheet(models.Model):
-    """
-        Here are the rights associated with the expense flow
-
-        Action       Group                   Restriction
-        =================================================================================
-        Submit      Employee                Only his own
-                    Officer                 If he is expense manager of the employee, manager of the employee
-                                             or the employee is in the department managed by the officer
-                    Manager                 Always
-        Approve     Officer                 Not his own and he is expense manager of the employee, manager of the employee
-                                             or the employee is in the department managed by the officer
-                    Manager                 Always
-        Post        Anybody                 State = approve and journal_id defined
-        Done        Anybody                 State = approve and journal_id defined
-        Cancel      Officer                 Not his own and he is expense manager of the employee, manager of the employee
-                                             or the employee is in the department managed by the officer
-                    Manager                 Always
-        =================================================================================
-    """
-
+    # _name = 'hr.expense.sheets'
     _inherit = ['hr.expense.sheet']
     
-    destination = fields.Char(string='Destination')
-    froms = fields.Char(string='From')
-    to = fields.Many2one('hr.expense',string='To')
+    user_id = fields.Many2one('res.users', 'Manager', compute='_compute_from_employee_id', store=True, readonly=True, copy=False, states={'draft': [('readonly', False)]}, tracking=True, domain=lambda self: [('groups_id', 'in', self.env.ref('hr_expense.group_hr_expense_team_approver').id)])
+    company_id = fields.Many2one('res.company', string='Company', required=True, readonly=True, states={'draft': [('readonly', False)]}, default=lambda self: self.env.company)
+    payment_mode = fields.Selection(related='expense_line_ids.payment_mode', default='own_account', readonly=True, string="Paid By", tracking=True)
+
+    employee_id = fields.Many2one()
+    applicant_code = fields.Char(default=lambda self:self.env.user.name)
+    barcode = fields.Char(string="Badge ID", help="ID used for employee identification.", groups="hr.group_hr_user", copy=False)
+    departement_code = fields.Char(string="Departemen Code")
+    expense_type = fields.Char(string="Expense Type")
+    account = fields.Char(string="Account")
+
+
+    attachment_number = fields.Integer('Number of Attachments', compute='_compute_attachment_number')
+
+    def action_submit_sheet(self):
+        if self.attachment_number == 0:
+            raise ValidationError(_("Attachments Required!"))
+        super(HrExpenseSheet, self).action_submit_sheet()
+    def action_register_payment(self):
+        if self.attachment_number == 0:
+            raise ValidationError(_('Attachment Required!!'))
